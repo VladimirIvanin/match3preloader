682fce7491bb3f57dd913d3c36e8022306df9436
vladimir.ivanin <vladimir.ivanin@insales.ru>
2024-11-02 18:21:40 +0300

    добавляем обработку тач событий

--- src/canvas-service.ts
+++ src/canvas-service.ts
@@ -39,26 +39,85 @@ export class CanvasService {
     this.handImage = new Image();
     this.isGemExchange = false;
 
-    this.canvas.addEventListener('mousemove', (event) => {
-      const rect = this.canvas.getBoundingClientRect();
-      this.hoverGemX = Math.floor((event.clientX - rect.left) / (CANVAS_SCALE * CELL_SIZE));
-      this.hoverGemY = this.boardHeight - 1 - Math.floor((event.clientY - rect.top) / (CANVAS_SCALE * CELL_SIZE));
-    });
-
-    this.canvas.addEventListener('mouseout', () => {
-      this.hoverGemX = undefined;
-      this.hoverGemY = undefined;
-    });
-
-    this.canvas.addEventListener('mousedown', () => {
-      this.activeGemX = this.hoverGemX;
-      this.activeGemY = this.hoverGemY;
-      this.isGemExchange = true;
-    });
-
-    this.canvas.addEventListener('mouseup', () => {
-    });
+    this.addEventListeners();
   }
+
+  private addEventListeners() {
+    this.canvas.addEventListener('mousemove', this.handlePointerMove.bind(this));
+    this.canvas.addEventListener('mouseout', this.handlePointerOut.bind(this));
+    this.canvas.addEventListener('mousedown', this.handlePointerDown.bind(this));
+    this.canvas.addEventListener('mouseup', this.handlePointerUp.bind(this));
+
+    this.canvas.addEventListener('touchmove', this.handleTouchMove.bind(this));
+    this.canvas.addEventListener('touchstart', this.handleTouchStart.bind(this));
+    this.canvas.addEventListener('touchend', this.handleTouchEnd.bind(this));
+  }
+
+  private handlePointerMove(event: MouseEvent) {
+    const { x, y } = this.getPointerPosition(event.clientX, event.clientY);
+    this.updateHoverPosition(x, y);
+  }
+
+  private handleTouchMove(event: TouchEvent) {
+    event.preventDefault();
+    const touch = event.touches[0];
+    const { x, y } = this.getPointerPosition(touch.clientX, touch.clientY);
+    this.updateHoverPosition(x, y);
+  }
+
+  private handlePointerOut() {
+    this.resetHoverPosition();
+  }
+
+  private handlePointerDown() {
+    this.setActivePosition();
+  }
+
+  private handleTouchStart(event: TouchEvent) {
+    event.preventDefault();
+    const touch = event.touches[0];
+    const { x, y } = this.getPointerPosition(touch.clientX, touch.clientY);
+    this.updateHoverPosition(x, y);
+    this.setActivePosition();
+  }
+
+  private handlePointerUp() {
+    this.resetActivePosition();
+  }
+
+  private handleTouchEnd(event: TouchEvent) {
+    event.preventDefault();
+    this.resetHoverPosition();
+    this.resetActivePosition();
+  }
+
+  private getPointerPosition(clientX: number, clientY: number) {
+    const rect = this.canvas.getBoundingClientRect();
+    const x = Math.floor((clientX - rect.left) / (CANVAS_SCALE * CELL_SIZE));
+    const y = this.boardHeight - 1 - Math.floor((clientY - rect.top) / (CANVAS_SCALE * CELL_SIZE));
+    return { x, y };
+  }
+
+  private updateHoverPosition(x: number, y: number) {
+    this.hoverGemX = x;
+    this.hoverGemY = y;
+  }
+
+  private resetHoverPosition() {
+    this.hoverGemX = undefined;
+    this.hoverGemY = undefined;
+  }
+
+  private setActivePosition() {
+    this.activeGemX = this.hoverGemX;
+    this.activeGemY = this.hoverGemY;
+    this.isGemExchange = true;
+  }
+
+  private resetActivePosition() {
+    this.isGemExchange = false;
+  }
+
   drawHand(x: number, y: number) {
     const scale = 0.8;
     const margin = (1 - scale) / 2;
