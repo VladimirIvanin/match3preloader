127d9b4ad85f523d3898d73fcc3c236763f6df98
vladimir.ivanin <vladimir.ivanin@insales.ru>
2024-11-12 17:41:00 +0300

    fix(build): удаляем генерацию типов для библиотеки Vue2 в конфиге

--- vite.config.vue2.ts
+++ vite.config.vue2.ts
@@ -1,10 +1,9 @@
 import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue2';
-import dts from 'vite-plugin-dts';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [vue(), dts({ outDir: 'dist/vue2' })],
+  plugins: [vue()],
   build: {
     outDir: 'dist/vue2',
     lib: {
