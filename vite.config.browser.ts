b4e40384eda6a06eaeefb49c517b0f141b67b33c
vladimir.ivanin <vladimir.ivanin@insales.ru>
2024-11-12 17:38:46 +0300

    fix(package): удаляем типы из конфигурации для браузерной сборки

--- vite.config.browser.ts
+++ vite.config.browser.ts
@@ -1,9 +1,7 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [dts({ outDir: 'dist/browser' })],
   build: {
     outDir: 'dist/browser',
     lib: {
